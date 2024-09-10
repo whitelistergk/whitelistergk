f9214b22dac9fafcfc445ff85f8e180084942bf6
ElFo2K <81543563+ElFo2K@users.noreply.github.com>
2024-09-10 19:56:17 +0200

    Update build.php

--- whitelists/build.php
+++ whitelists/build.php
@@ -1,9 +1,13 @@
 <?php
-return array(
+$whitelist = array(
     "whitelist" => array(
         "ElFo2K",
         "SoyFire",
         "ImClassicMc",
         "ItsNavii"
-    )
+    ),
 );
+
+header('Content-Type: application/json');
+echo json_encode($whitelist);
+?>
